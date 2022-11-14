c8c4ae3c044e2e256eef2357d67aa293ab398e74
ashikmahmud <ashikmamud187@gmail.com>
2022-11-15 00:21:00 +0600

    done to show notification items

--- src/shared/Header.tsx
+++ src/shared/Header.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { AiOutlineBell } from "react-icons/ai";
 import { BsChatSquareDots } from "react-icons/bs";
 type Props = {};
 
 const Header = (props: Props) => {
+  const [showNotifications, setShowNotifications] = useState(false);
+
   return (
     <div>
       <div className="flex items-center justify-between bg-white mb-3 p-2 px-8">
@@ -10,29 +13,64 @@ const Header = (props: Props) => {
           <BsChatSquareDots size={30} /> Real Chat.
         </div>
         <div className="flex items-center gap-5">
-          <div className="notification">
-            <div className="notification-icon cursor-pointer relative">
+          <div className="notification relative">
+            <div
+              className="notification-icon cursor-pointer relative"
+              onClick={() => setShowNotifications((state) => !state)}
+            >
               <AiOutlineBell size={30} />
               <div className="dot absolute w-5 h-5 text-xs rounded-full grid place-items-center bg-blue-500 text-blue-100 -top-1 -right-1">
                 4
               </div>
             </div>
+            {showNotifications && (
+              <div className="absolute right-0 top-10 bg-white w-48 rounded-lg shadow-lg p-3 flex flex-col gap-2 border sm:w-[15rem]">
+                <div className="flex items-center gap-2  shadow hover:bg-slate-100 p-2 rounded-lg cursor-pointer">
+                  <img
+                    src="https://i.pravatar.cc/150?img=1"
+                    alt=""
+                    className="rounded-full w-8 h-8"
+                  />
+                  <div className="flex items-start flex-col">
+                    <span className="text-sky-500 font-bold">John Doe</span>
+                    <span className="text-gray-500 flex items-center text-xs  gap-1">
+                      send you a message
+                    </span>
+                  </div>
+                </div>
+                <div className="flex items-center gap-2  shadow hover:bg-slate-100 p-2 rounded-lg cursor-pointer">
+                  <img
+                    src="https://i.pravatar.cc/150?img=1"
+                    alt=""
+                    className="rounded-full w-8 h-8"
+                  />
+                  <div className="flex items-start flex-col">
+                    <span className="text-sky-500 font-bold">John Doe</span>
+                    <span className="text-gray-500 flex items-center text-xs  gap-1">
+                      send you a message
+                    </span>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
           {/* notification end */}
-          <div className="flex items-center gap-2 bg-gray-50 p-2 rounded-lg">
-            <div className="bg-sky-100 text-sky-500 p-2 rounded-lg">
-              <img
-                src="https://i.pravatar.cc/150?img=1"
-                alt=""
-                className="rounded-full w-8 h-8"
-              />
-            </div>
-            <div className="flex items-start flex-col">
-              <span className="text-sky-500 font-bold">John Doe</span>
-              <span className="text-gray-500 flex items-center text-xs  gap-1">
-                <i className=" w-2 h-2 block rounded-full bg-green-500"></i>{" "}
-                Online
-              </span>
+          <div className="relative">
+            <div className="flex items-center gap-2 bg-gray-50 p-2 rounded-lg">
+              <div className="bg-sky-100 text-sky-500 p-2 rounded-lg">
+                <img
+                  src="https://i.pravatar.cc/150?img=1"
+                  alt=""
+                  className="rounded-full w-8 h-8"
+                />
+              </div>
+              <div className="flex items-start flex-col cursor-pointer">
+                <span className="text-sky-500 font-bold">John Doe</span>
+                <span className="text-gray-500 flex items-center text-xs  gap-1">
+                  <i className=" w-2 h-2 block rounded-full bg-green-500"></i>{" "}
+                  Online
+                </span>
+              </div>
             </div>
           </div>
         </div>
