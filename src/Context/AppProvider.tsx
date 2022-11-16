cf0c6ab5e5732349a2ef58650653ca8996979040
ashikmahmud <ashikmamud187@gmail.com>
2022-11-17 00:28:20 +0600

    create context api for handling auth state

---
+++ src/Context/AppProvider.tsx
@@ -0,0 +1,33 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import Cookie from "universal-cookie";
+const AppContext = createContext(null);
+const cookie = new Cookie();
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const AppProvider = ({ children }: Props) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const user = cookie.get("user");
+    if (user) {
+      setUser(user);
+    }
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ user, setUser } as any}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export default AppProvider;
+
+const useAppContext = () => {
+  return useContext(AppContext);
+};
+
+export { useAppContext };
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./app/store";
+import AppProvider from "./Context/AppProvider";
 import "./index.css";
 
 import reportWebVitals from "./reportWebVitals";
@@ -10,11 +10,11 @@ const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
+  <AppProvider>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </AppProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
