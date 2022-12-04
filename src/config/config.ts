b6917036c4685328d4efb479f90d19bab3e2aa82
ashikmahmud <ashikmamud187@gmail.com>
2022-12-04 21:18:47 +0600

    change live server link

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,4 @@
-export const server_url = `https://real-chat-app.onrender.com/api`;
+export const server_url = `https://real-chat-app-server.onrender.com/api`;
+// export const server_url = `https://real-chat-app.onrender.com/api`;
 // export const server_url = `http://localhost:5000/api`;
 export const upload_api = `e3ea60acc20d26f77d269beff59767e2`;
